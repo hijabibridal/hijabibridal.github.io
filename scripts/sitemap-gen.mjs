ccc0a75a57be9bdd0eb28d5e937a700d63bc53ec
hijabibridal <nick.garciapgi@gmail.com>
2026-02-22 10:38:40 -0500

    Update sitemap automation and lastmod dates

--- scripts/sitemap-gen.mjs
+++ scripts/sitemap-gen.mjs
@@ -1,20 +1,51 @@
 import fs from 'fs';
-// Change 'assert' to 'with' here:
 import productData from '../src/data/bridal-products.json' with { type: 'json' };
 import blogData from '../src/data/blog-articles.json' with { type: 'json' };
 
 const BASE_URL = 'https://hijabibridal.github.io';
 
+// Get today's date in YYYY-MM-DD format for the sitemap
+const today = new Date().toISOString().split('T')[0];
+
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url><loc>${BASE_URL}/</loc></url>
-  <url><loc>${BASE_URL}/shop</loc></url>
-  <url><loc>${BASE_URL}/blog</loc></url>
-  ${productData.products.map(p => `  <url><loc>${BASE_URL}/shop/product/${p.slug}</loc></url>`).join('\n')}
-  ${productData.mainCategories.map(c => `  <url><loc>${BASE_URL}/shop/category/${c.slug}</loc></url>`).join('\n')}
-  ${blogData.articles.map(a => `  <url><loc>${BASE_URL}/blog/${a.slug}</loc></url>`).join('\n')}
+  <url>
+    <loc>${BASE_URL}/</loc>
+    <lastmod>${today}</lastmod>
+  </url>
+  <url>
+    <loc>${BASE_URL}/shop</loc>
+    <lastmod>${today}</lastmod>
+  </url>
+  <url>
+    <loc>${BASE_URL}/blog</loc>
+    <lastmod>${today}</lastmod>
+  </url>
+  
+  {/* 1. Shop Product Pages */}
+  ${productData.products.map(p => `  <url>
+    <loc>${BASE_URL}/shop/product/${p.slug}</loc>
+    <lastmod>${today}</lastmod>
+  </url>`).join('\n')}
+  
+  {/* 2. Shop Category Pages */}
+  ${productData.mainCategories.map(c => `  <url>
+    <loc>${BASE_URL}/shop/category/${c.slug}</loc>
+    <lastmod>${today}</lastmod>
+  </url>`).join('\n')}
+  
+  {/* 3. Blog Category Pages */}
+  ${blogData.mainCategories.map(bc => `  <url>
+    <loc>${BASE_URL}/blog/category/${bc.slug}</loc>
+    <lastmod>${today}</lastmod>
+  </url>`).join('\n')}
+  
+  {/* 4. Individual Blog Articles */}
+  ${blogData.articles.map(a => `  <url>
+    <loc>${BASE_URL}/blog/${a.slug}</loc>
+    <lastmod>${today}</lastmod>
+  </url>`).join('\n')}
 </urlset>`;
 
-// Change the last line to:
 fs.writeFileSync('./public/sitemap_index.xml', sitemap);
-console.log('✅ Sitemap updated in /public as sitemap_index.xml');
+console.log('✅ Sitemap updated with lastmod dates in /public/sitemap_index.xml');
