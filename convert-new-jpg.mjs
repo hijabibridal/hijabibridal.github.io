bac2c15bb3053d1a5fcd20e3c136df6d924a959e
hijabibridal <nick.garciapgi@gmail.com>
2026-03-05 11:39:02 -0500

    white groom outfits

--- convert-new-jpg.mjs
+++ convert-new-jpg.mjs
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import sharp from 'sharp';
 
-const TARGET_DIR = './public/images/halal-nails';
+const TARGET_DIR = './public/images/muslim-groom-outfit';
 
 async function convertJpgToWebp() {
   const files = fs.readdirSync(TARGET_DIR);
