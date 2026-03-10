1ea253f44ec2af2b7970454c80a0b0374a918cf4
hijabibridal <nick.garciapgi@gmail.com>
2026-03-10 16:17:50 -0400

    image sitemap

--- scripts/sitemap-gen.mjs
+++ scripts/sitemap-gen.mjs
@@ -6,28 +6,44 @@ const BASE_URL = 'https://hijabibridal.github.io';
 const today = new Date().toISOString().split('T')[0];
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
   <url><loc>${BASE_URL}/</loc><lastmod>${today}</lastmod></url>
   <url><loc>${BASE_URL}/shop</loc><lastmod>${today}</lastmod></url>
   <url><loc>${BASE_URL}/blog</loc><lastmod>${today}</lastmod></url>
   
-  ${productData.products.map(p => `  <url><loc>${BASE_URL}/shop/product/${p.slug}</loc><lastmod>${today}</lastmod></url>`).join('\n')}
+  ${productData.products.map(p => `
+  <url>
+    <loc>${BASE_URL}/shop/product/${p.slug}</loc>
+    <lastmod>${today}</lastmod>
+    ${p.images.map(img => `
+    <image:image>
+      <image:loc>${BASE_URL}/images/${img.url.replace(/^\//, '')}</image:loc>
+      <image:title>${p.name}</image:title>
+      <image:caption>${img.alt || p.name}</image:caption>
+    </image:image>`).join('')}
+  </url>`).join('\n')}
   
   ${productData.mainCategories.map(c => `  <url><loc>${BASE_URL}/shop/category/${c.slug}</loc><lastmod>${today}</lastmod></url>`).join('\n')}
   
   ${blogData.mainCategories.map(bc => `  <url><loc>${BASE_URL}/blog/category/${bc.slug}</loc><lastmod>${today}</lastmod></url>`).join('\n')}
   
-  ${blogData.articles.map(a => `  <url><loc>${BASE_URL}/blog/${a.slug}</loc><lastmod>${today}</lastmod></url>`).join('\n')}
+  ${blogData.articles.map(a => `
+  <url>
+    <loc>${BASE_URL}/blog/${a.slug}</loc>
+    <lastmod>${today}</lastmod>
+    ${a.featuredImageUrl ? `
+    <image:image>
+      <image:loc>${BASE_URL}${a.featuredImageUrl}</image:loc>
+      <image:title>${a.pageTitle}</image:title>
+    </image:image>` : ''}
+  </url>`).join('\n')}
 </urlset>`;
 
-// 1. Define the directory path
 const dir = './public/sitemap';
-
-// 2. Create the directory if it doesn't exist
 if (!fs.existsSync(dir)){
     fs.mkdirSync(dir, { recursive: true });
 }
 
-// 3. Write the file to the new location
 fs.writeFileSync(`${dir}/sitemap.xml`, sitemap);
-console.log('✅ Sitemap generated at ./public/sitemap/sitemap.xml');
+console.log('✅ Sitemap with Images generated at ./public/sitemap/sitemap.xml');
